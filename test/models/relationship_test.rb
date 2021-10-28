edb9b57d543e6e393d0800ec58bbb7bfc07019c0
acaiibawl <hagiya0719@gmail.com>
2021-10-28 14:06:14 +0900

    コミット忘れ

--- test/models/relationship_test.rb
+++ test/models/relationship_test.rb
@@ -4,7 +4,7 @@ class RelationshipTest < ActiveSupport::TestCase
   def setup
     @relationship = Relationship.new(follower_id: users(:michael).id, followed_id: users(:archer).id)
   end
-
+ 
   test 'should be valid' do
     assert @relationship.valid?
   end
